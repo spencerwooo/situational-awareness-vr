3d3dfd720bf940d4a2c18ecac55a68da2b58f419
spencerwooo <spencer.woo@outlook.com>
2021-09-28 13:36:58 +0100

    Remove controller input system check

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
-#endif
 
 /* Note: animations are called via the controller for both the character and capsule using animator null checks
  */
@@ -9,9 +7,8 @@ using UnityEngine.InputSystem;
 namespace StarterAssets
 {
   [RequireComponent(typeof(CharacterController))]
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
   [RequireComponent(typeof(PlayerInput))]
-#endif
+
   public class FirstPersonController : MonoBehaviour
   {
     [Header("Player")]
