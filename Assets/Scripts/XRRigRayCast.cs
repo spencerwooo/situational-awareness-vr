7c9acdb532ba0e4654ea4ad0321bffbbf7ea5777
spencerwooo <spencer.woo@outlook.com>
2021-10-04 22:46:12 +0100

    Add player activity logs to the game UI

--- Assets/Scripts/XRRigRayCast.cs
+++ Assets/Scripts/XRRigRayCast.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class XRRigRayCast : MonoBehaviour
 {
+  public float sampleRate = 2.0f;
+  public TextMeshProUGUI hitObjectText;
+  public TextMeshProUGUI hitPointText;
+  public TextMeshProUGUI distanceText;
+  public TextMeshProUGUI framePerSecText;
+  public TextMeshProUGUI playerPositionText;
+  public TextMeshProUGUI playerOrientationText;
+
   private GameObject mainCamera;
   private float rayLength = 30.0f;
   private RaycastHit vision;
@@ -19,6 +28,7 @@ public class XRRigRayCast : MonoBehaviour
   private void FixedUpdate()
   {
     HeadMountRaycast();
+    UpdatePlayerInteractionData();
   }
 
   private void HeadMountRaycast()
@@ -33,10 +43,24 @@ public class XRRigRayCast : MonoBehaviour
 
     if (Physics.Raycast(headsetPos, headsetDir, out vision, rayLength, layerMask))
     {
-      if (vision.collider.CompareTag("Interactive"))
-      {
-        Debug.Log(vision.collider.name);
-      }
+      hitObjectText.text = vision.collider.name;
+      hitPointText.text = string.Format("[{0:0.00}, {1:0.00}, {2:0.00}]", vision.point.x, vision.point.y, vision.point.z);
+      distanceText.text = string.Format("{0:0.00}", vision.distance);
     }
+  }
+
+  private void UpdatePlayerInteractionData()
+  {
+    Vector3 playerPosition = mainCamera.transform.position;
+    Vector3 playerOrientation = mainCamera.transform.forward;
+
+    framePerSecText.text = string.Format("{0}", getFramePerSec());
+    playerPositionText.text = string.Format("[{0:0.00}, {1:0.00}, {2:0.00}]", playerPosition.x, playerPosition.y, playerPosition.z);
+    playerOrientationText.text = string.Format("[{0:0.00}, {1:0.00}, {2:0.00}]", playerOrientation.x, playerOrientation.y, playerOrientation.z);
+  }
+
+  private float getFramePerSec()
+  {
+    return 1.0f / Time.deltaTime;
   }
 }
