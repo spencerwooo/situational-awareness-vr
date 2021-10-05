db6fd27045fe8ca437bd52315b8bbcb3a7dbeb50
spencerwooo <spencer.woo@outlook.com>
2021-10-05 15:57:33 +0100

    Add XR rig and hand controllers respectively

---
+++ Assets/Scripts/XRHandController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRHandController : MonoBehaviour
+{
+  // https://forum.unity.com/threads/any-example-of-the-new-2019-1-xr-input-system.629824/#post-4360375
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}
