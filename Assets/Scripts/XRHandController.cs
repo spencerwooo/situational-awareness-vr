508c4e43638c96118f1d703e68437f673599c1d4
spencerwooo <spencer.woo@outlook.com>
2021-10-05 20:10:58 +0100

    Add door state scriptable object

--- Assets/Scripts/XRHandController.cs
+++ Assets/Scripts/XRHandController.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRHandController : MonoBehaviour
 {
+  // https://forum.unity.com/threads/door-open-close-in-c.410811/
   // https://forum.unity.com/threads/any-example-of-the-new-2019-1-xr-input-system.629824/#post-4360375
+  public bool isTriggerPressed;
+  public ActionBasedController controller;
+
   void Start()
   {
-
+    if (controller == null)
+    {
+      controller = GetComponent<ActionBasedController>();
+    }
   }
 
   void Update()
   {
-
+    // Debug.Log(controller.selectAction.action.ReadValue<bool>());
   }
 }
