2b5eea29e72e526eab6e9fd8c7e6c5f83acd4281
spencerwooo <spencer.woo@outlook.com>
2021-11-07 13:55:34 +0000

    Plot out 3D coordinates inside Unity scene

--- Assets/Scripts/VisualiseCoords.cs
+++ Assets/Scripts/VisualiseCoords.cs
@@ -14,6 +14,11 @@ public class VisualiseCoords : MonoBehaviour
     private static void OnRuntimeMethodLoad()
     {
         var data = CsvReader.Read("InteractionLogs-211020-125559");
+
+        Material userPositionMaterial = Resources.Load<Material>("Materials/UserPositionSphere");
+        Material camHitPointMaterial = Resources.Load<Material>("Materials/CamHitPointSphere");
+        Material controllerHitPointMaterial = Resources.Load<Material>("Materials/ControllerHitPointSphere");
+
         for (var i = 0; i < data.Count; i++)
         {
             // CSV dictionary keys include:
@@ -23,23 +28,41 @@ public class VisualiseCoords : MonoBehaviour
             // - controller_hit_obj, controller_hit_point, controller_hit_distance
 
             Debug.Log(data[i]["user_position"]);
-            // TODO: bug here
-            // Vector3 userPosition = ParseVector3(data[i]["user_position"].ToString());
-            // GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            // sphere.transform.position = userPosition;
 
-            if (i > 10) break;
+            Vector3 userPosition = ParseVector3(data[i]["user_position"].ToString());
+            RenderCoordinate(userPosition, userPositionMaterial);
+
+            Vector3 camHitPoint = ParseVector3(data[i]["camera_hit_point"].ToString());
+            RenderCoordinate(camHitPoint, camHitPointMaterial);
+
+            Vector3 controllerHitPoint = ParseVector3(data[i]["controller_hit_point"].ToString());
+            RenderCoordinate(controllerHitPoint, controllerHitPointMaterial);
+
+            // if (i > 100) break;
         }
 
         Debug.Log("Loaded CSV.");
     }
 
+    private static void RenderCoordinate(Vector3 coord, Material mat)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = coord;
+        sphere.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        Renderer sphereRenderer = sphere.GetComponent<Renderer>();
+        sphereRenderer.material = mat;
+    }
+
     private static Vector3 ParseVector3(string coordinates)
     {
-        string[] coordXYZ = coordinates.Split(',');
+        // Coordinates look like (xx, yy, zz), here we parse the coordinate string by removing
+        // the leading '(' and trailing ')', and split the string to floats
+        string[] coordXYZ = coordinates.TrimStart('(').TrimEnd(')').Split(',');
+
         float coordX = float.Parse(coordXYZ[0]);
         float coordY = float.Parse(coordXYZ[1]);
         float coordZ = float.Parse(coordXYZ[2]);
+
         return new Vector3(coordX, coordY, coordZ);
     }
 }
