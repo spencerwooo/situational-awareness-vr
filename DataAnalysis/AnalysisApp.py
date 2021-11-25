5e6a0d32092de77d4bca859b72fb5bd30d6538a0
spencerwooo <spencer.woo@outlook.com>
2021-11-25 13:10:02 +0800

    Plotly interactive dashboard

--- DataAnalysis/AnalysisApp.py
+++ DataAnalysis/AnalysisApp.py
@@ -10,43 +10,88 @@ import plotly.express as px
 from dash import dash_table, dcc, html
 from dash.dependencies import Input, Output, State
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+external_stylesheets = ["https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(
     [
-        dcc.Upload(
-            id="upload-csv",
-            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-            style={
-                "height": "80px",
-                "lineHeight": "80px",
-                "borderWidth": "2px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            multiple=False,
+        html.Header(
+            className="text-gray-600 body-font",
+            children=html.Div(
+                [
+                    html.A(
+                        [
+                            html.Img(src="https://img.icons8.com/color/96/000000/cosine.png", className="h-10 w-10"),
+                            html.Span("Visualisation and Analysis", className="ml-3 text-xl"),
+                        ],
+                        className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0",
+                    ),
+                    dcc.Upload(
+                        id="upload-csv",
+                        children=html.Button(
+                            "Upload file",
+                            className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none "
+                            "hover:bg-gray-200 rounded text-base mt-4 md:mt-0",
+                        ),
+                        multiple=True,
+                    ),
+                ],
+                className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between",
+            ),
         ),
-        html.Div(id="output-visualise"),
-    ]
+        html.Div(id="output-visualise", className="container mx-auto p-5 flex flex-col flex-1 space-y-4"),
+        html.Footer(
+            "Situational Awareness Visualisation and Analysis ©️2021 - Shangbo Wu",
+            className="p-5 text-sm text-gray-400 bg-gray-50 text-center",
+        ),
+    ],
+    className="h-screen flex flex-col",
 )
 
 
 def parse_uploaded_file(contents, filename, date):
-    content_type, content_string = contents.split(",")
-    decoded = base64.b64decode(content_string)
+    _, content_string_0 = contents[0].split(",")
+    _, content_string_1 = contents[1].split(",")
+    decoded_0 = base64.b64decode(content_string_0)
+    decoded_1 = base64.b64decode(content_string_1)
 
     try:
-        if "csv" in filename:
-            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
+        if "csv" in filename[0] and "txt" in filename[1]:
+            df = pd.read_csv(io.StringIO(decoded_0.decode("utf-8")))
+            txt_content = decoded_1.decode("utf-8")
+            csv_filename, txt_filename = filename
+            csv_date, txt_date = date
+        elif "csv" in filename[1] and "txt" in filename[0]:
+            df = pd.read_csv(io.StringIO(decoded_1.decode("utf-8")))
+            txt_content = decoded_0.decode("utf-8")
+            txt_filename, csv_filename = filename
+            txt_date, csv_date = date
+
     except Exception as e:
         print(e)
         return html.Div(["There was an error parsing the file."])
 
-    cam_dist_hist = px.histogram(df, x="camera_hit_dist", marginal="rug", nbins=50)
-    con_dist_hist = px.histogram(df, x="controller_hit_distance", marginal="rug", nbins=50)
+    def duration_parser(delta):
+        t, f = delta.split(".")
+        h, m, s = t.split(":")
+        return f"{int(h) * 60 * 60 + int(m) * 60 + int(s)}.{f}"
+
+    summary = [line.split(":", 1)[-1] for line in txt_content.splitlines()]
+    time_spent = [summary[0], summary[2], summary[4]]
+    time_spent = pd.DataFrame(
+        [duration_parser(t) for t in time_spent], index=["Room 1", "Room 2", "Room 3"], columns=["time"], dtype=float
+    )
+    start_frame = [summary[1], summary[3], summary[5]]
+    start_frame = [int(f) for f in start_frame]
+
+    time_benchmark = px.bar(time_spent, y=time_spent.index, x="time", color=time_spent.index)
+
+    cam_dist_hist = px.histogram(
+        df, x="camera_hit_dist", marginal="rug", nbins=50, title="Camera attention distance to player"
+    )
+    con_dist_hist = px.histogram(
+        df, x="controller_hit_distance", marginal="rug", nbins=50, title="Controller attention to player"
+    )
 
     cam_attention_count = (
         df.groupby("camera_hit_obj")
@@ -55,7 +100,13 @@ def parse_uploaded_file(contents, filename, date):
         .rename(columns={"frame_no": "count"})
         .sort_values(["count"], ascending=False)
     )
-    cam_attention = px.bar(cam_attention_count, x="camera_hit_obj", y="count", color="camera_hit_obj")
+    cam_attention = px.bar(
+        cam_attention_count,
+        x="camera_hit_obj",
+        y="count",
+        color="camera_hit_obj",
+        title="Camera attention game objects",
+    )
     con_attention_count = (
         df.groupby("controller_hit_obj")
         .count()
@@ -63,15 +114,30 @@ def parse_uploaded_file(contents, filename, date):
         .rename(columns={"frame_no": "count"})
         .sort_values(["count"], ascending=False)
     )
-    con_attention = px.bar(con_attention_count, x="controller_hit_obj", y="count", color="controller_hit_obj")
+    con_attention = px.bar(
+        con_attention_count,
+        x="controller_hit_obj",
+        y="count",
+        color="controller_hit_obj",
+        title="Controller attention game objects",
+    )
 
     return html.Div(
         [
-            html.Div(f"{filename} - {datetime.fromtimestamp(date)}"),
-            html.H6(),
+            html.Div(f"{csv_filename} - {datetime.fromtimestamp(csv_date)}"),
             dash_table.DataTable(
-                data=df.head(100).to_dict("records"), columns=[{"name": i, "id": i} for i in df.columns], page_size=10
+                data=df.head(100).to_dict("records"), columns=[{"name": i, "id": i} for i in df.columns], page_size=6
             ),
+            html.Div(f"{txt_filename} - {datetime.fromtimestamp(txt_date)}"),
+            html.Div(
+                txt_content,
+                className="block whitespace-pre overflow-x-scroll font-mono text-xs border bg-gray-100 p-2 rounded",
+            ),
+            dcc.Graph(id="time_benchmark", figure=time_benchmark),
+            html.Hr(),
+            html.Div("Top-down Coordinate Visualisation"),
+            html.Hr(),
+            html.Div("Interactive 3D Scatter Visualisation"),
             html.Hr(),
             html.Div(
                 [
@@ -87,9 +153,6 @@ def parse_uploaded_file(contents, filename, date):
                 ],
                 style={"display": "flex"},
             ),
-            html.Hr(),
-            html.Div("Raw Content"),
-            html.Pre(contents[0:200] + "...", style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"}),
         ]
     )
 
@@ -102,7 +165,21 @@ def parse_uploaded_file(contents, filename, date):
 )
 def update_output(content, name, date):
     if content is not None:
-        return parse_uploaded_file(content, name, date)
+        if len(content) == 2:
+            return parse_uploaded_file(content, name, date)
+        else:
+            return html.Div(
+                "CSV and TXT files should be uploaded together.",
+                className="flex flex-1 flex-col items-center justify-center font-bold text-red-400",
+            )
+    else:
+        return html.Div(
+            [
+                html.P("Waiting for upload ...", className="font-bold"),
+                html.P("Please upload the CSV and the TXT files together.", className="text-gray-300"),
+            ],
+            className="flex flex-1 flex-col items-center justify-center",
+        )
 
 
 if __name__ == "__main__":
